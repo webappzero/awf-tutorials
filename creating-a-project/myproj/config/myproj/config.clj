a8557cdfeca21a65d6eaf871f5c2a5a08c7cc4d1
Jay Martin <james.martin7@gmail.com>
2017-03-28 13:27:11 -0400

    First commit

---
+++ creating-a-project/myproj/config/myproj/config.clj
@@ -0,0 +1,11 @@
+(ns ^:config myproj.config
+  (:require [arachne.core.dsl :as a]))
+
+
+(a/id :myproj/widget-1 (a/component 'myproj.core/make-widget))
+
+
+(a/id :myproj/runtime (a/runtime [:myproj/widget-1]))
+
+
+
